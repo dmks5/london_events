22d59a31ae0a39004d914721e9acc86d6e6b196e
dmks5 <diman.mit@gmail.com>
2023-11-06 22:48:54 +0000

    Deploy React app to GitHub Pages

---
+++ precache-manifest.768a5c60dc046ecafca625c69d4963c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6594670939d0c4402e9a27eb2ab4eb04",
+    "url": "/london_events/index.html"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/london_events/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/london_events/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/london_events/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/london_events/static/js/main.01b01939.chunk.js"
+  },
+  {
+    "revision": "83ad943ed482998a1cfb",
+    "url": "/london_events/static/js/runtime~main.fa8dd8cf.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/london_events/static/media/logo.06e73328.svg"
+  }
+]);
